24a1ef233304294242b5a960dd782501b25e7160
Ochirsaikhan <davaajambal01@allegheny.edu>
2023-02-13 15:12:35 -0500

    Update site meta data

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,15 +1,14 @@
 module.exports = {
   url: process.env.URL || 'http://localhost:8080',
-  siteName: 'Eleventy Excellent',
-  siteDescription:
-    'Eleventy starter based on the workflow suggested by buildexcellentwebsit.es.',
+  siteName: 'Ochir | Data Analytics',
+  siteDescription: 'This website is for CMPSC*301 Data Analytics class',
   siteType: 'Person', // schema
   locale: 'en_EN',
   lang: 'en',
   skipContent: 'Skip to content',
-  author: 'Lene Saile', // i.e. Lene Saile - author's name. Must be set.
+  author: 'Ochirsaikhan', // author's name. Must be set.
   authorEmail: '', // i.e. hola@lenesaile.com - email of the author
-  authorWebsite: '', // i.e. https.://www.lenesaile.com - the personal site of the author
+  authorWebsite: 'ochirsaikhan.github.io', // the personal site of the author
   themeColor: '#DD4462', //  Manifest: defines the default theme color for the application
   themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
